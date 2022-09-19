ee4b2bef06717f9c0359f27bac250dde105a7231
Daniel White <92924+whited@users.noreply.github.com>
2022-09-19 15:48:06 -0400

    Compiling for kb2040

---
+++ keyboards/splitkb/kyria/keymaps/whited/rules.mk
@@ -0,0 +1,24 @@
+MCU = RP2040
+BOOTLOADER = rp2040
+
+# allow warnings until they get fixed for rp2040
+ALLOW_WARNINGS = yes
+
+# LTO must be disabled for RP2040 builds
+LTO_ENABLE = no
+
+SERIAL_DRIVER = vendor
+WS2812_DRIVER = vendor  # to allow PIO
+
+# Audio driver does not work for rp2040
+AUDIO_ENABLE = no
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+# ENCODER_MAP_ENABLE = yes
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+MOUSEKEY_ENABLE = yes
+
+# convert to kb2040 pinout
+CONVERT_TO = kb2040
